222de0ef0480b4ec05eacb841345331ce644dbc6
huyf <yufan.hu@thoughtworks.com>
2020-03-22 13:57:59 +0800

    [huyufan] feat: value never less than 0.

---
+++ src/main/java/cn/xpbootcamp/gilded_rose/Commodity.java
@@ -0,0 +1,30 @@
+package cn.xpbootcamp.gilded_rose;
+
+public class Commodity {
+
+    private int shelfLife;
+    private int quality;
+
+    public Commodity(int shelfLife, int quality) {
+        this.shelfLife = shelfLife;
+        this.quality = quality;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void save(int days) {
+        int normalReduceValue;
+        int doubleReduceValue = 0;
+        if (days <= this.shelfLife) {
+            normalReduceValue = days;
+        } else {
+            normalReduceValue = this.shelfLife;
+            doubleReduceValue = (days - this.shelfLife) * 2;
+        }
+
+        int checkedQuality = this.quality - normalReduceValue - doubleReduceValue;
+        this.quality = checkedQuality < 0 ? 0 : checkedQuality;
+    }
+}
